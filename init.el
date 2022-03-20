40cb1cba92ce58c3b3a94515ebfaf215c5e80962
zomboy002 <zomboy001.mail@gmail.com>
2022-03-20 11:10:51 +0800

    Configuration

--- init.el
+++ init.el
@@ -31,16 +31,3 @@
                   (org-level-7 . 1.2)
                   (org-level-8 . 1.2)))
     (set-face-attribute (car face) nil :font "FiraCode Nerd Font" :weight 'medium :height (cdr face))))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(general vterm evil-collection which-key yasnippet sudo-edit writeroom-mode marginalia consult orderless vertico company htmlize ox-reveal treemacs rainbow-delimiters rainbow-mode gcmh fancy-battery doom-modeline dashboard beacon all-the-icons-dired doom-themes org-superstar org-roam evil-org diminish use-package)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
